693372e0074bdee6170e3a6e126913725a98c297
Sarge <63020240+sargedev@users.noreply.github.com>
2024-08-31 01:43:50 +0200

    Added custom test code

--- test.ts
+++ test.ts
@@ -2,7 +2,6 @@ game.consoleOverlay.setVisible(true)
 control.runInParallel(function() {
     console.log(`connecting to https://www.websocket.org/echo.html`)
 
-    {
         // string tests
         const ws = new WebSocket("wss://echo.websocket.org")
         ws.onerror = () => console.log("error")
@@ -11,32 +10,6 @@ control.runInParallel(function() {
             console.log(`--> ${data}`)
         }
         ws.onopen = () => {
-            forever(() => {
-                const m = `makecode ${control.millis()}`;
-                console.log(`<-- ${m}`)
-                ws.send(m);    
-                pause(1000)
-            })
+            ws.send("makecode test");
         }    
-    }
-
-    pause(500)
-    
-    {
-        // binary tests
-        const ws = new WebSocket("wss://echo.websocket.org")
-        ws.onerror = () => console.log("error")
-        ws.onmessage = (msg) => {
-            const data = msg.data;
-            console.log(`--> ${(data as Buffer).toHex()}`)
-        }
-        ws.onopen = () => {
-            forever(() => {
-                const b = Buffer.fromArray([control.millis()]);
-                console.log(`<-- ${b.toHex()}`)
-                ws.send(b);
-                pause(1000)
-            })
-        } 
-    }
 })
