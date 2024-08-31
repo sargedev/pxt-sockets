ea0f3d23931539d4b2acd5af03b70c36cd878806
Sarge <63020240+sargedev@users.noreply.github.com>
2024-08-31 15:53:09 +0200

    Added custom ngrok domain

--- test.ts
+++ test.ts
@@ -1,17 +1,17 @@
 game.consoleOverlay.setVisible(true)
 control.runInParallel(function() {
-    console.log(`connecting to https://www.websocket.org/echo.html`)
+    console.log(`connecting to http://hopeful-doe-notably.ngrok-free.app/`)
 
-        // string tests
-        const ws = new WebSocket("wss://echo.websocket.org")
-        ws.onerror = () => console.log("error")
-        ws.onmessage = (msg) => {
-            const data = msg.data;
-            console.log(`[Recieved] ${data}`)
-        }
-        ws.onopen = () => {
-            const msg = "Sarge test";
-            ws.send(msg);
-            console.log(`[Sent] ${msg}`);
-        }    
+    // string tests
+    const ws = new WebSocket("ws://hopeful-doe-notably.ngrok-free.app")
+    ws.onerror = () => console.log("error")
+    ws.onmessage = (msg) => {
+        const data = msg.data;
+        console.log(`[Recieved] ${data}`)
+    }
+    ws.onopen = () => {
+        const msg = "Sarge test";
+        ws.send(msg);
+        console.log(`[Sent] ${msg}`);
+    }    
 })
