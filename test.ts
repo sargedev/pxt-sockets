9f963c972b7713ff7368e535a3b71d252048cf8a
Sarge <63020240+sargedev@users.noreply.github.com>
2024-09-03 12:05:00 +0200

    Changed URL

--- test.ts
+++ test.ts
@@ -1,10 +1,14 @@
+
+const version = "0.0.6";
+
 game.consoleOverlay.setVisible(true)
 control.runInParallel(function() {
-    console.log(`connecting to http://hopeful-doe-notably.ngrok-free.app/`)
+
+    console.log(`WebSockets test v${version}`);
 
     // string tests
-    const ws = new WebSocket("wss://hopeful-doe-notably.ngrok-free.app")
-    ws.onerror = () => console.log("error")
+    const ws = new WebSocket("wss://makecode-websocket-server.glitch.me")
+    ws.onerror = (e: Event) => console.log("error")
     ws.onmessage = (msg) => {
         const data = msg.data;
         console.log(`[Recieved] ${data}`)
