4d853f8a0e76adc8927e4bc63f74fd0378483928
Sarge <63020240+sargedev@users.noreply.github.com>
2024-09-11 19:31:02 +0200

    Changed to DDNS domain

--- test.ts
+++ test.ts
@@ -1,13 +1,15 @@
 
-const version = "0.0.6";
+const version = "0.0.7";
+const domain = "makecodelive.ddns.net:443"
 
 game.consoleOverlay.setVisible(true)
 control.runInParallel(function() {
 
     console.log(`WebSockets test v${version}`);
+    console.log(`Connecting to '${domain}'`)
 
     // string tests
-    const ws = new WebSocket("wss://makecode-websocket-server.glitch.me")
+    const ws = new WebSocket(`wss://${domain}`)
     ws.onerror = (e: Event) => console.log("error")
     ws.onmessage = (msg) => {
         const data = msg.data;
