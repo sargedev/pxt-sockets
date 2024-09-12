cc9c99b4feea53af02bec5e5ae4bca3998d60a7f
Sarge <63020240+sargedev@users.noreply.github.com>
2024-09-12 19:17:20 +0200

    Finished menu structure

--- interface.ts
+++ interface.ts
@@ -7,35 +7,81 @@ enum Menu {
 
 let menu = Menu.Main;
 blockMenu.setColors(6, 0);
+showMainMenu();
 
-function showMainMenu() {
-    blockMenu.showMenu(["Sign Up", "Log In"], MenuStyle.List, MenuLocation.BottomHalf);
+function showMainMenu(index: number=0) {
+    blockMenu.showMenu(["Log In", "Create Account"], MenuStyle.List, MenuLocation.BottomHalf);
     menu = Menu.Main;
+    blockMenu.setSelectedIndex(index);
 }
 
-function showCreateAccountMenu(args: {username?: string, password?: string, confirm?: string}) {
+function showCreateAccountMenu(index: number=0) {
     let options = [
-        args.username || "Username",
-        args.password || "Password",
-        args.confirm || "Confirm Password",
+        username || "Username",
+        password || "Password",
+        confirm || "Confirm Password",
         "Create Account",
         "Back"
     ]
     blockMenu.showMenu(options, MenuStyle.List, MenuLocation.BottomHalf);
     menu = Menu.CreateAccount;
+    blockMenu.setSelectedIndex(index);
 }
 
-function showLoginMenu(args: {username?: string, password?: string}) {
+function showLoginMenu(index: number=0) {
     let options = [
-        args.username || "Username",
-        args.password || "Password",
+        username || "Username",
+        password || "Password",
         "Log In",
         "Back"
     ]
     blockMenu.showMenu(options, MenuStyle.List, MenuLocation.BottomHalf);
     menu = Menu.LogIn;
+    blockMenu.setSelectedIndex(index);
 }
 
-blockMenu.onMenuOptionSelected((option: string, index: number) => {
+let username: string;
+let password: string;
+let confirm: string;
 
+blockMenu.onMenuOptionSelected((option: string, index: number) => {
+    blockMenu.closeMenu();
+    if (menu === Menu.Main) {
+        if (index === 0) showLoginMenu();
+        else if (index === 1) showCreateAccountMenu();
+    } else if (menu === Menu.LogIn) {
+        if (index === 0) {
+            username = game.askForString("Enter Username", 24);
+            showLoginMenu(index);
+        } else if (index === 1) {
+            password = game.askForString("Enter Password", 24);
+            showLoginMenu(index);
+        } else if (index === 2) {
+            // authenticate
+            showLoginMenu(index);
+        } else if (index === 3) {
+            username = "";
+            password = "";
+            showMainMenu();
+        }
+    } else if (menu === Menu.CreateAccount) {
+        if (index === 0) {
+            username = game.askForString("Enter Username", 24);
+            showCreateAccountMenu(index);
+        } else if (index === 1) {
+            password = game.askForString("Enter Password", 24);
+            showCreateAccountMenu(index);
+        } else if (index === 2) {
+            confirm = game.askForString("Confirm Password", 24);
+            showCreateAccountMenu(index);
+        } else if (index === 3) {
+            // create account
+            showCreateAccountMenu(index);
+        } else if (index === 4) {
+            username = "";
+            password = "";
+            confirm = "";
+            showMainMenu();
+        }
+    }
 })
