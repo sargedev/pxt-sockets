849d7c949ef3d609a71ea04ad3bb76313351eef6
Sarge <63020240+sargedev@users.noreply.github.com>
2024-09-17 19:15:11 +0200

    Reverted changes and wrote simple test case

--- netcode.ts
+++ netcode.ts
@@ -1,60 +1,22 @@
 
+const version = "0.2.0";
+const domain = "makecodelive.ddns.net:443";
 game.consoleOverlay.setVisible(true);
 
-const ERR_EMPTY_RESPONSE = "Response is empty or invalid";
-const ERR_INVALID_RESPONSE = "Invalid response";
+console.log(`WebSockets test v${version}`);
 
-interface Response { [key: string]: any };
-interface Result {
-    response?: Response;
-    error?: string;
-}
+const client = new WebSocket(`wss://${domain}`);
+console.log("Connecting to server...");
 
-class SocketHandler {
-    socket: WebSocket;
-    handlers: ((response: Response) => any)[];
-    
-    constructor(socket: WebSocket) {
-        this.socket = socket;
-        this.handlers = [];
-        socket.onmessage = (msg) => this.broadcast(msg);
-    }
-
-    subscribe(handler: (response: Response) => any) {
-        this.handlers.push(handler);
-    }
-
-    private validate(msg: MessageEvent): Result {
-        let json = JSON.stringify(msg.data as string) as Response;
-        if (!json) return {error: ERR_EMPTY_RESPONSE};
-        if (!json["action"] || !json["response"]) return {error: ERR_INVALID_RESPONSE};
-        return {response: json};
-    }
-
-    private broadcast(msg: MessageEvent) {
-        let response = this.validate(msg);
-        this.handlers.forEach((value) => value(response));
-    }
-}
-
-let dispatcher: SocketHandler;
 control.runInParallel(() => {
-    const connection = new WebSocket("wss://makecodelive.ddns.net:443");
-    dispatcher = new SocketHandler(connection);
-})
+    client.onopen = () => {
+        console.log("Connected to server");
 
-pause(0);
+        client.send("Test message");
+        console.log("[Sent] Test message");
 
-let start = game.runtime();
-dispatcher.socket.onopen = () => dispatcher.socket.send(JSON.stringify({
-    action: "handshake",
-    data: {}
-}))
-console.log("Sent handshake");
-
-dispatcher.subscribe((res) => {
-    console.log("Recieved packet " + JSON.stringify(res));
-    if (res["action"] === "handshake" && res["response"] === "success") {
-        console.log(`Connected to server in ${game.runtime() - start}ms`)
+        client.onmessage = (msg) => {
+            console.log(`[Server] ${msg}`);
+        }
     }
 })
