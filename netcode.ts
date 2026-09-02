d01b6c6ae715010b89364ff0fe7fc59fc707841d
Sarge <63020240+sargedev@users.noreply.github.com>
2026-09-02 10:30:19 +0200

    Small tweaks

--- netcode.ts
+++ netcode.ts
@@ -1,12 +1,20 @@
 
-const version = "0.2.2";
+namespace userconfig {
+    export const ARCADE_SCREEN_WIDTH = 320;
+    export const ARCADE_SCREEN_HEIGHT = 240;
+}
+
+const version = "0.2.3";
 const domain = "hopeful-doe-notably.ngrok-free.app";
 game.consoleOverlay.setVisible(true);
 
 console.log(`WebSockets test v${version}`);
+console.log("Press A to connect to server");
+
+controller.A.pauseUntil(ControllerButtonEvent.Pressed);
 
 const client = new WebSocket(`wss://${domain}`);
-console.log("Connecting to server...");
+console.log(`Connecting to ${domain}...`);
 
 control.runInParallel(() => {
     client.onopen = () => {
@@ -14,6 +22,10 @@ control.runInParallel(() => {
 
         client.send("Test message");
         console.log("[Sent] Test message");
+    }
+
+    client.onerror = (e) => {
+        console.log(e);
     }
 
     client.onmessage = (msg) => {
